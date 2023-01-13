73366a9247dda47f51c8bede53d4ea4590140485
Louis-2204 <louis.lamouche2204@gmail.com>
2023-01-13 13:11:36 +0100

    fix erreur message register

--- index.php
+++ index.php
@@ -82,7 +82,9 @@ $unControleur = new Controleur($serveur, $bdd, $user, $mdp);
 
             $unUser = $unControleur->Register($tab);
             if ($unUser == null) {
-                echo "<div class='col-md-3 alert alert-danger'>Problème technique, réessayer plus tard</div>";
+                echo "<div class='col-md-3 alert alert-danger'>Erreur technique veuillez réessayer plus tard<span onclick='closeAlertDanger()'> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x-lg' viewBox='0 0 16 16'>
+            <path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z'/>
+          </svg> </span> </div>";
             } else {
                 $_SESSION['email'] = $unUser['email_e'];
                 $_SESSION['nom'] = $unUser['nom_e'];
