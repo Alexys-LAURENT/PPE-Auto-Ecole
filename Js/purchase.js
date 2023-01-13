ce5fe25c1d36b4ec8be91136969490702a60b0c1
Alexys-LAURENT <alexyslaurent.442@gmail.com>
2023-01-13 09:45:08 +0100

    timer on succed purchase

--- Js/purchase.js
+++ Js/purchase.js
@@ -7,20 +7,48 @@ document.getElementById('submit').addEventListener('click', function () {
 
     if (nomCarte.value == '') {
         nomCarte.style.cssText = 'border : 1px solid red;';
+    } else {
+        nomCarte.style.cssText = 'border : 1px solid green;';
     }
 
     if (numeroCarte.value == '') {
         numeroCarte.style.cssText = 'border : 1px solid red;';
+    } else {
+        numeroCarte.style.cssText = 'border : 1px solid green;';
     }
 
 
     if (dateCarte.value == '') {
         dateCarte.style.cssText = 'border : 1px solid red;';
+    } else {
+        dateCarte.style.cssText = 'border : 1px solid green;';
     }
 
 
     if (cvvCarte.value == '') {
         cvvCarte.style.cssText = 'border : 1px solid red;';
+    } else {
+        cvvCarte.style.cssText = 'border : 1px solid green;';
     }
 
+});
+
+
+document.getElementById('submit').addEventListener('click', function () {
+    if (nomCarte.value == '' || numeroCarte.value == '' || dateCarte.value == '' || cvvCarte.value == '') {
+        event.preventDefault();
+    } else {
+        event.preventDefault();
+
+        document.getElementById('textbtnSublit').style.display = 'none';
+        document.getElementById('gif').style.display = 'block';
+        document.getElementById('flash').style.display = 'block';
+
+        setTimeout(function () {
+            document.location.href = "index.php?page=2";
+        }, 5000);
+
+
+        // alert('Votre commande a bien été prise en compte');
+    }
 });
--- purchase.php
+++ purchase.php
@@ -3,6 +3,10 @@ session_start();
 if (!isset($_SESSION['email'])) {
     header("Location: index.php?page=0");
 }
+
+// if (isset($_POST['submitPurchase']) && isset($_POST['nom']) && isset($_POST['num']) && isset($_POST['dateExpi']) && isset($_POST['cvv'])) {
+//     header('Location: index.php?page=2');
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,37 +20,45 @@ if (!isset($_SESSION['email'])) {
 </head>
 
 <body>
+    <div id="flash">
+        <p>Merci pour votre achat, vous allez être redirigé dans un instant...</p>
+    </div>
+
     <div class="body">
         <div class="container">
-            <div class="header">
-                <h3>Paiement en ligne</h3>
-            </div>
-            <div class="main">
-                <div class="cards">
-                    <img src="./images/visa.png" width="50px" height="18px" alt="">
-                    <img class="mastercard" src="./images/mastercard.png" width="50px" alt="">
+            <form action="">
+                <div class="header">
+                    <h3>Paiement en ligne</h3>
                 </div>
-                <h2>Montant du paiement</h2>
-                <p id="montant">XXX €</p>
+                <div class="main">
+                    <div class="cards">
+                        <img src="./images/visa.png" width="50px" height="18px" alt="">
+                        <img class="mastercard" src="./images/mastercard.png" width="50px" alt="">
+                    </div>
+                    <h2>Montant du paiement</h2>
+                    <p id="montant">XXX €</p>
 
-                <label for="">Nom sur la carte :</label>
-                <input type="text" name="nom" id="inputNom">
+                    <label for="">Nom sur la carte :</label>
+                    <input type="text" name="nom" id="inputNom">
 
 
 
-                <label for="">Numéro de carte :</label>
-                <input type="text" name="num" id="inputNum">
+                    <label for="">Numéro de carte :</label>
+                    <input type="text" name="num" id="inputNum">
 
-                <div class="d-flex-label">
-                    <label for="">Date d'éxpiration</label>
-                    <label for="">CVV</label>
+                    <div class="d-flex-label">
+                        <label for="">Date d'éxpiration</label>
+                        <label for="">CVV</label>
+                    </div>
+                    <div class="d-flex input">
+                        <input type="text" placeholder="MM/YY" id="inputDate" name="dateExpi">
+                        <input type="number" maxlength="3" id="inputCVV" name="cvv">
+                    </div>
+                    <button type="submit" name="submitPurchase" id="submit">
+                        <p id="textbtnSublit">payer</p><img id="gif" src="./images/loading.gif" alt="">
+                    </button>
                 </div>
-                <div class="d-flex input">
-                    <input type="text" placeholder="MM/YY" id="inputDate">
-                    <input type="number" maxlength="3" id="inputCVV">
-                </div>
-                <button type="submit" id="submit">payer</button>
-            </div>
+            </form>
         </div>
     </div>
 
