51394937877cdfaf8f7a835668bb8ca6ab739b76
Louis-2204 <louis.lamouche2204@gmail.com>
2023-05-23 00:20:17 +0200

    mariadb

--- forfaits.php
+++ forfaits.php
@@ -36,21 +36,6 @@
         }
     }
 
-
-    if (isset($_POST['Submit-Code'])) {
-        if (!isset($_SESSION['User'])) { //Si pas connecté, redirect login/register
-            $_SESSION['redirection'] = "index.php?page=1"; // on set une variable de session qui est un lien de redirection
-            header("location: index.php?page=9"); // cette variable de session sera utiliser dans l'accueil puis "unset" pour revenir à cette page après inscription/connexion
-        } else if (!empty($_SESSION['formation'])) { //Si connecté et déjà inscrit à une formation
-            echo "<div class='col-md-3 alert alert-danger'>Vous avez déjà une formation<span onclick='closeAlertDanger()'> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x-lg' viewBox='0 0 16 16'>
-                <path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z'/>
-              </svg> </span> </div>";
-        } else {
-            $_SESSION['achat'] = true;
-            header("location: index.php?page=99&offre=Code");
-        }
-    }
-
     ?>
     <!DOCTYPE html>
     <html lang="fr">
